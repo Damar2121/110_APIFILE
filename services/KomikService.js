6cc7bd3aa32890788f8df41817bce82a1e4e17e8
Damar2121 <damar.vivoy51@gmail.com>
2025-11-26 11:13:27 +0700

    revisi code di KomikService.js

--- services/KomikService.js
+++ services/KomikService.js
@@ -39,4 +39,30 @@ async function getKomikById(database, id) {
     return komik;
 }
 
-module.exports = { createKomik, getAllKomik, getKomikById };
+async function updateKomik(database, id, komikData) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) {
+        throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    }
+
+    await komik.update(komikData);
+    return komik;
+}
+
+async function deleteKomik(database, id) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) {
+        throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    }
+
+    await komik.destroy();
+    return { message: `Komik dengan ID ${id} berhasil dihapus` };
+}
+
+module.exports = {
+    createKomik,
+    getAllKomik,
+    getKomikById,
+    updateKomik,
+    deleteKomik,
+};
