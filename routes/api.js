556b79b77a1141b95c2439ceaf3aa712c4dda1e8
Damar2121 <damar.vivoy51@gmail.com>
2025-11-26 11:37:39 +0700

    finalisasi

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const KomikController = require('../controllers/komikController');
-const upload = multer({ storage: multer.memoryStorage()});
+const komikController = require('../controllers/KomikController');
+const upload = multer({ storage: multer.memoryStorage() });
 
-router.post('/komik', upload.single('gambar'), KomikController.createKomik);
-router.get('/komik', KomikController.getAllKomiks);
-router.get('/komik/:id', KomikController.getKomikById);
-router.put('/komik/:id', upload.single('gambar'), KomikController.updateKomik);
-router.delete('/komik/:id', KomikController.deleteKomik);
+
+router.get('/komik', komikController.getAllKomik);
+router.get('/komik/:id', komikController.getKomikById);
+router.post('/komik', upload.single('gambar'), komikController.createKomik);
+router.put('/komik/:id', upload.single('gambar'), komikController.updateKomik);
+router.delete('/komik/:id', komikController.deleteKomik);
 
 module.exports = router;
